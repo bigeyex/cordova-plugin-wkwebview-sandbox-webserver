98258a98b0eb0ccbe09373f7937f46cf4d6e993b
Gavin Li <flashcode2000@qq.com>
2018-09-23 15:11:04 +0800

    modified ios code

--- src/ios/CDVWKWebviewSandboxWebServer.m
+++ src/ios/CDVWKWebviewSandboxWebServer.m
@@ -39,78 +39,10 @@
 @implementation CDVWKWebviewSandboxWebServer
 
 - (void) pluginInitialize {
+    self.server = [[GCDWebServer alloc]init];
+    [self.server addGETHandlerForBasePath:@"/" directoryPath:NSHomeDirectory() indexFilename:nil cacheAge:3600 allowRangeRequests:YES];
+    [self.server startWithPort:49086 bonjourName:nil];
 
-    BOOL useLocalWebServer = NO;
-    BOOL requirementsOK = NO;
-    NSString* indexPage = @"index.html";
-    NSString* appBasePath = @"www";
-    NSUInteger port = 80;
-
-    // check the content tag src
-    CDVViewController* vc = (CDVViewController*)self.viewController;
-    NSURL* startPageUrl = [NSURL URLWithString:vc.startPage];
-    if (startPageUrl != nil) {
-        if ([[startPageUrl scheme] isEqualToString:@"http"] && [[startPageUrl host] isEqualToString:@"localhost"]) {
-            port = [[startPageUrl port] unsignedIntegerValue];
-            useLocalWebServer = YES;
-        }
-    }
-
-    requirementsOK = [self checkRequirements];
-    if (!requirementsOK) {
-        useLocalWebServer = NO;
-        NSString* alternateContentSrc = [self.commandDelegate.settings cordovaSettingForKey:@"AlternateContentSrc"];
-        vc.startPage = alternateContentSrc? alternateContentSrc : indexPage;
-    }
-
-    // check setting
-#if TARGET_IPHONE_SIMULATOR
-    if (useLocalWebServer) {
-        NSNumber* startOnSimulatorSetting = [[self.commandDelegate settings] objectForKey:[@"CordovaLocalWebServerStartOnSimulator" lowercaseString]];
-        if (startOnSimulatorSetting) {
-            useLocalWebServer = [startOnSimulatorSetting boolValue];
-        }
-    }
-#endif
-    
-    if (port == 0) {
-        // CB-9096 - actually test for an available port, and set it explicitly
-        port = [self _availablePort];
-    }
-
-    NSString* authToken = [NSString stringWithFormat:@"cdvToken=%@", [[NSProcessInfo processInfo] globallyUniqueString]];
-
-    self.server = [[GCDWebServer alloc] init];
-    [GCDWebServer setLogLevel:kGCDWebServerLoggingLevel_Error];
-
-    if (useLocalWebServer) {
-        [self addAppFileSystemHandler:authToken basePath:[NSString stringWithFormat:@"/%@/", appBasePath] indexPage:indexPage];
-
-        // add after server is started to get the true port
-        [self addFileSystemHandlers:authToken];
-        [self addErrorSystemHandler:authToken];
-        
-        // handlers must be added before server starts
-        [self.server startWithPort:port bonjourName:nil];
-
-        // Update the startPage (supported in cordova-ios 3.7.0, see https://issues.apache.org/jira/browse/CB-7857)
-		vc.startPage = [NSString stringWithFormat:@"http://localhost:%lu/%@/%@?%@", (unsigned long)self.server.port, appBasePath, indexPage, authToken];
-
-    } else {
-        if (requirementsOK) {
-            NSString* error = [NSString stringWithFormat:@"WARNING: CordovaLocalWebServer: <content> tag src is not http://localhost[:port] (is %@).", vc.startPage];
-            NSLog(@"%@", error);
-
-            [self addErrorSystemHandler:authToken];
-            
-            // handlers must be added before server starts
-            [self.server startWithPort:port bonjourName:nil];
-
-            vc.startPage = [self createErrorUrl:error authToken:authToken];
-        } else {
-            GWS_LOG_ERROR(@"%@ stopped, failed requirements check.", [self.server class]);
-        }
-    }
 }
 
 
